9fd1bf71c0c41e102a6f52f8c60f191307866ca2
markrobertvandam <markrobertvandam@gmail.com>
2021-11-05 16:15:32 +0100

    finished test-queries.py

--- test-queries.py
+++ test-queries.py
@@ -1,43 +1,76 @@
+from numpy import average
 from SPARQLWrapper import SPARQLWrapper
+from sklearn.metrics import f1_score
+import argparse
+import pickle
 import time
 
-class Query_tester(object):
+
+class QueryTester(object):
     """ Object for testing phase """
 
-    def __init__(self, query_output: str, test_output: str = None, golden_standard: str = None) -> None:
-        self.test_output = test_output
-        self.golden_standard = golden_standard
+    def __init__(self, test_queries: str, query_output: str, golden_standard: str = None) -> None:
+        self.test_queries = test_queries
         self.query_output = query_output
-        self.sparql = SPARQLWrapper("https://query.wikidata.org/")
+        self.golden_standard = golden_standard
+        self.sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+        self.sparql.setReturnFormat("json")
 
     def resolve_queries(self) -> None:
-        """Requests the queries in test_output"""
-        input_file = open(self.test_output, 'r')
-        output_file = open(self.query_output, 'w+')
+        """Requests the queries in test_queries and saves the results"""
+        input_file = open(self.test_queries, 'r', encoding='UTF-8')
+        output_file = open(self.query_output, 'w+', encoding='UTF-8')
+
+        pickle_list = []
         for query in input_file.readlines():
             self.sparql.setQuery(query)
             try:
-                ret = self.sparql.query()
-                print(ret)
+                ret = self.sparql.query().convert()
+                answer_list = ret["results"]["bindings"]
+                answer_uris = []
+                for answer in answer_list:
+                    answer_uris.append(answer["uri"]["value"])
+                output_file.write(str(answer_uris) + '\n')
+                pickle_list.append(answer_uris)
                 # ret is a stream with the results in XML, see <http://www.w3.org/TR/rdf-sparql-XMLres/>
             except Exception as e:
                 print(f"ERROR WITH QUERY: {e}")
-                ret = f"ERROR WITH QUERY: {e}"
-            time.sleep(20)
+            time.sleep(1)
+        pickle.dump(pickle_list, open(f"{self.query_output[:-3]}p", "wb"))
 
     def compare_queries(self):
-        self.resolve_queries()
-        query_outputs = open(self.query_output, 'r').readlines()
-        golden_standard = open(self.golden_standard, 'r').readlines()
+        if self.test_queries:
+            self.resolve_queries()
+            query_outputs = pickle.load(open(f"{self.query_output[:-3]}p", "rb"))
+        else:
+            query_outputs = pickle.load(open(self.query_output, "rb"))
+        golden_standard = pickle.load(open(self.golden_standard, "rb"))
 
         # normal accuracy
         correct = 0
         for i in range(len(query_outputs)):
-            if query_outputs[i] == golden_standard[i]:
+            y_pred = query_outputs[i]
+            y_true = golden_standard[i]
+            if y_pred == y_true:
                 correct += 1
+            print(average(f1_score(y_true, y_pred, average=None)))
         accuracy = correct / len(query_outputs)
 
-obj = Query_tester("data/results.txt", "data/input.txt", "data/output.txt")
-obj.resolve_queries()
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--test_queries", dest="test_queries")
+    parser.add_argument("--query_output", dest="query_output", required=True)
+    parser.add_argument("--golden_standard", dest="golden_standard")
+    args = parser.parse_args()
+    obj = QueryTester(args.test_queries, args.query_output, args.golden_standard)
+    if obj.golden_standard:
+        obj.compare_queries()
+    else:
+        obj.resolve_queries()
+
+
+if __name__ == '__main__':
+    main()
 
 
