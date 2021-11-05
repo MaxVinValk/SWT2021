5c6d9a62e6ac3026fa397fd055cf1f04a20c8319
Max Valk <maxvalk@pop-os.localdomain>
2021-11-05 10:45:21 +0100

    Unsquished file

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ def main_train(args):
     model = EncoderDecoder(device=args.device, beam_size=10, max_length=64)
 
     model.freeze_params()
+    model.to(args.device)
+    model.train()
 
     data_loader = get_loader(
         f"{args.data_folder}/{args.data_filename}",
@@ -22,46 +24,23 @@ def main_train(args):
         model.decoder_tokenizer,
     )
 
-    model.to(args.device)
-    model.train()
-
     # The parameters we do not wish to optimize
     no_decay = ["bias", "LayerNorm.weight"]
 
     optimizer_grouped_parameters = [
-        {
-            "params": [
-                p
-                for n, p in model.named_parameters()
-                if not any(nd in n for nd in no_decay)
-            ],
-            "weight_decay": args.weight_decay,
-        },
-        {
-            "params": [
-                p
-                for n, p in model.named_parameters()
-                if any(nd in n for nd in no_decay)
-            ],
-            "weight_decay": 0.0,
-        },
+        {"params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
+         "weight_decay": args.weight_decay},
+        {"params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], "weight_decay": 0.0}
     ]
-    t_total = len(data_loader) // args.gradient_accumulation_steps * args.train_steps
-    optimizer = AdamW(
-        optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon
-    )
-    scheduler = get_linear_schedule_with_warmup(
-        optimizer, num_warmup_steps=int(t_total * 0.1), num_training_steps=t_total
-    )
 
-    nb_tr_examples, nb_tr_steps, tr_loss, global_step, best_bleu, best_loss = (
-        0,
-        0,
-        0,
-        0,
-        0,
-        1e6,
-    )
+    t_total = len(data_loader) // args.gradient_accumulation_steps * args.train_steps
+    optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
+    scheduler = get_linear_schedule_with_warmup(optimizer,
+                                                num_warmup_steps=int(t_total * 0.1),
+                                                num_training_steps=t_total)
+
+    nb_tr_examples, nb_tr_steps, tr_loss, global_step, best_bleu, best_loss = 0, 0, 0, 0, 0, 1e6
+
     for epoch in range(args.train_steps):
         bar = tqdm(data_loader, total=len(data_loader))
 
@@ -106,10 +85,8 @@ def main_exp(device):
     model.to(device)
 
     source_sentence = "How many movies did Stanley Kubrick direct?"
-    target_sentence = (
-        "SELECT DISTINCT COUNT(?uri) WHERE {?uri <http://dbpedia.org/ontology/director> "
-        "<http://dbpedia.org/resource/Stanley_Kubrick>  . } "
-    )
+    target_sentence = "SELECT DISTINCT COUNT(?uri) WHERE {?uri <http://dbpedia.org/ontology/director> " \
+                      "<http://dbpedia.org/resource/Stanley_Kubrick>  . } "
 
     source_ids, source_mask, _, _ = encode_single_example(
         source_sentence,
@@ -134,9 +111,7 @@ def main_exp(device):
         if 0 in output:
             output = output[: output.index(0)]
 
-        sentence = model.decoder_tokenizer.decode(
-            output, clean_up_tokenization_spaces=False
-        )
+        sentence = model.decoder_tokenizer.decode(output, clean_up_tokenization_spaces=False)
         given_results.append(sentence)
 
     print(f"\n\nSource sentence: {source_sentence}")
@@ -147,7 +122,6 @@ def main_exp(device):
     for i in range(len(given_results)):
         print(f"{i + 1}: {given_results[i]}")
 
-        # Print reverted queries
         print("--------------")
         print(revert_query(given_results[i]))
 
